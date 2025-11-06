f1d1591a86ae3915340bf972df1a42ccee9416b7
Tên của bạn <buivanduy476gmail.com>
2025-11-06 07:46:11 +0700

    acv

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
     document.addEventListener('DOMContentLoaded', function() {
         const hamburgerMenu = document.querySelector('.hamburger-menu');
@@ -8,3 +9,7 @@
             mainNav.classList.toggle('active');
         });
     })
+=======
+const datausers=document.querySelectorAll(".form");
+console.log(datausers);
+>>>>>>> 9143aca09f4a749bc863b6fec037d570371824dd
