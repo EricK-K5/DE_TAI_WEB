4124a3dbcd5f837a7e379ab79babede877798eca
tui-la-duy <buivanduy476@gmail.com>
2025-11-23 17:18:41 +0700

    Update DK.js

--- JS/DK.js
+++ JS/DK.js
@@ -73,3 +73,10 @@ form.addEventListener('submit',(e)=>{
 
     }    
 })
+var hien_mk=document.getElementById('hien_mk');
+hien_mk.addEventListener('change',(e)=>{
+    if (e.target.checked)
+        document.getElementById('password').type='text';
+    else
+        document.getElementById('password').type="password";
+})
