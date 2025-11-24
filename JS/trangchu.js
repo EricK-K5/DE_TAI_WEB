e14320a6a4f130eeeeaf5837d2d1b0f4d4db2b2f
benjamin-vn <baob2408776@student.ctu.edu.vn>
2025-11-24 08:05:51 +0700

    Add files via upload

---
+++ JS/trangchu.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const addCartButtons = document.querySelectorAll(".add-cart-btn");
+  const cartCount = document.getElementById("cart-count");
+  const popup = document.getElementById("popup-cart");
+
+  let totalItems = 0;
+
+  addCartButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      const productCard = e.target.closest(".bestSellerItem");
+      const qtyInput = productCard.querySelector(".quantity-input");
+      const quantity = parseInt(qtyInput.value);
+
+      totalItems += quantity;
+      cartCount.textContent = totalItems;
+
+      showPopup("Sản phẩm đã được thêm vào giỏ hàng!");
+    });
+  });
+})
