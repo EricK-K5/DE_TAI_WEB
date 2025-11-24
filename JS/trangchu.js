efab84818a239d6b76cd21d8a7965fa84654ce79
EricK-K5 <huynhtrikhoanguyen@gmail.com>
2025-11-24 22:01:57 +0700

    sua

--- JS/trangchu.js
+++ JS/trangchu.js
@@ -1,27 +1,27 @@
-var photo1 = document.getElementById("photo1a");
-photo1.addEventListener("click", () => {
-  document.getElementById("photo1").style.display = "inline";
-});
-var icon1 = document.getElementById("icon1");
-icon1.addEventListener("click", () => {
-  document.getElementById("photo1").style.display = "none";
-});
+var photo1=document.getElementById('photo1a')
+photo1.addEventListener('click',()=>{
+  document.getElementById('photo1').style.display='inline';
+})
+var icon1=document.getElementById('icon1');
+icon1.addEventListener('click',()=>{
+  document.getElementById('photo1').style.display='none';
+})
 
 //hinh2
-var photo2 = document.getElementById("photo2a");
-photo2.addEventListener("click", () => {
-  document.getElementById("photo2").style.display = "inline";
-});
-var icon2 = document.getElementById("icon2");
-icon2.addEventListener("click", () => {
-  document.getElementById("photo2").style.display = "none";
-});
+var photo2=document.getElementById('photo2a')
+photo2.addEventListener('click',()=>{
+  document.getElementById('photo2').style.display='inline';
+})
+var icon2=document.getElementById('icon2');
+icon2.addEventListener('click',()=>{
+  document.getElementById('photo2').style.display='none';
+})
 //hinh 3
-var photo3 = document.getElementById("photo3a");
-photo3.addEventListener("click", () => {
-  document.getElementById("photo3").style.display = "inline";
-});
-var icon3 = document.getElementById("icon3");
-icon3.addEventListener("click", () => {
-  document.getElementById("photo3").style.display = "none";
-});
+var photo3=document.getElementById('photo3a')
+photo3.addEventListener('click',()=>{
+  document.getElementById('photo3').style.display='inline';
+})
+var icon3=document.getElementById('icon3');
+icon3.addEventListener('click',()=>{
+  document.getElementById('photo3').style.display='none';
+})
