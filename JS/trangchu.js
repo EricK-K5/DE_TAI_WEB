ace0f42e2f545fa842f62f569bdb786aa5d669c2
tui-la-duy <buivanduy476@gmail.com>
2025-11-24 20:31:21 +0700

    Update trangchu.js

--- JS/trangchu.js
+++ JS/trangchu.js
@@ -1,21 +1,27 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const addCartButtons = document.querySelectorAll(".add-cart-btn");
-  const cartCount = document.getElementById("cart-count");
-  const popup = document.getElementById("popup-cart");
+var photo1=document.getElementById('photo1a')
+photo1.addEventListener('click',()=>{
+  document.getElementById('photo1').style.display='inline';
+})
+var icon1=document.getElementById('icon1');
+icon1.addEventListener('click',()=>{
+  document.getElementById('photo1').style.display='none';
+})
 
-  let totalItems = 0;
-
-  addCartButtons.forEach((button) => {
-    button.addEventListener("click", (e) => {
-      const productCard = e.target.closest(".bestSellerItem");
-      const qtyInput = productCard.querySelector(".quantity-input");
-      const quantity = parseInt(qtyInput.value);
-
-      totalItems += quantity;
-      cartCount.textContent = totalItems;
-
-      showPopup("Sản phẩm đã được thêm vào giỏ hàng!");
-      addToCart(productCard, quantity);
-    });
-  });
-});
+//hinh2
+var photo2=document.getElementById('photo2a')
+photo2.addEventListener('click',()=>{
+  document.getElementById('photo2').style.display='inline';
+})
+var icon2=document.getElementById('icon2');
+icon2.addEventListener('click',()=>{
+  document.getElementById('photo2').style.display='none';
+})
+//hinh 3
+var photo3=document.getElementById('photo3a')
+photo3.addEventListener('click',()=>{
+  document.getElementById('photo3').style.display='inline';
+})
+var icon3=document.getElementById('icon3');
+icon3.addEventListener('click',()=>{
+  document.getElementById('photo3').style.display='none';
+})
